413241683cfa24b6c2aa4dd616783e9e8eea3d6c
Somya Jha <m2somya@gmail.com>
2024-08-10 22:22:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# PaddleBounceC17
+# PaddleBounce
+
+### Game in JavaScript using p5-play.js
+
+
+#### Paddle moves with arrow keys (UP and DOWN).
+#### The ball has a score counter for a +1 every time it hits the paddle.
+#### Game Lost when ball touches the edge under the paddle.
+
+> github.com/thestormbreaks/PaddleBounce
--- sketch.js
+++ sketch.js
@@ -1,42 +1,56 @@
-var ball,img,paddle,paddle_img;
+var ball, img, paddle, paddle_img;
+var score = 0; // Initialize the score counter
+
 function preload() {
-  img=loadImage("ball.png")
-  paddle_img=loadImage("paddle.png")
-  
+  img = loadImage("ball.png");
+  paddle_img = loadImage("paddle.png");
 }
+
 function setup() {
   createCanvas(400, 400);
-  ball=createSprite(200,200,10,10);
-  ball.addImage("ball",img);
-  paddle=createSprite(350,200,10,10);
+  ball = createSprite(200, 200, 10, 10);
+  ball.addImage("ball", img);
+  paddle = createSprite(350, 200, 10, 10);
   paddle.addImage(paddle_img);
-  ball.velocityX= 9;
-
+  ball.velocityX = 9;
 }
 
 function draw() {
-  background(205,153,0);
-  edges=createEdgeSprites();
-  ball.bounceOff(edges);
-  ball.bounceOff(paddle,randomVelocity);
+  background(205, 153, 0);
+  edges = createEdgeSprites();
 
+  ball.bounceOff(edges[0]); // Left edge
+  ball.bounceOff(edges[2]); // Top edge
+  ball.bounceOff(edges[3]); // Right edge
   
-  
-  if(keyDown(UP_ARROW)){
-    paddle.y=paddle.y-20
-     
+  ball.bounceOff(paddle, randomVelocity);
+
+  // Check if the ball touches the bottom edge (under the paddle)
+  if (ball.isTouching(edges[1])) { // Bottom edge
+    textSize(32);
+    fill(255, 0, 0);
+    text("Game Lost", 120, 200);
+    noLoop(); // Stop the draw loop to end the game
   }
-  
-  if(keyDown(DOWN_ARROW)){
-    paddle.y=paddle.y+20
-    }
-  
-    drawSprites();
-  
+
+  if (keyDown(UP_ARROW)) {
+    paddle.y = paddle.y - 20;
+  }
+
+  if (keyDown(DOWN_ARROW)) {
+    paddle.y = paddle.y + 20;
+  }
+
+  drawSprites();
+
+  // Display the score
+  textSize(24);
+  fill(0);
+  text("Score: " + score, 10, 30);
 }
 
-function randomVelocity(){
-  ball.velocityY=random(+5,-5)
-
+// Modify the randomVelocity function to increase the score
+function randomVelocity() {
+  ball.velocityY = random(-5, 5);
+  score++; // Increment the score by 1 every time the ball bounces off the paddle
 }
-
